af7364f1eab58a44d68620898aae6bcaae1e5281
bonismo <bonismo@hotmail.com>
2019-08-19 22:49:35 +0800

    Init Commit

--- SpringBasic/src/test/java/StartTest.java
+++ SpringBasic/src/test/java/StartTest.java
@@ -1,7 +1,11 @@
 import io.stayhungrystayfoolish.aop.domain.User;
 import io.stayhungrystayfoolish.aop.service.UserService;
+import org.junit.Before;
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
 
 /**
  * @Author: bonismo@hotmail.com
@@ -11,13 +15,42 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class StartTest {
 
-    public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("aop/applicationContext.xml");
-        UserService userService = (UserService) context.getBean("userServiceImpl");
+    private ApplicationContext context;
+
+    private UserService userService;
+
+    @Before
+    public void init() {
+        context = new ClassPathXmlApplicationContext("aop/applicationContext.xml");
+        userService = (UserService) context.getBean("userServiceImpl");
+    }
+
+    @Test
+    public void save() {
         User user = new User();
         user.setName("bonismo");
         user.setAge(18);
         user = userService.save(user);
+        System.out.println(user);
+    }
+
+    @Test
+    public void findById() {
+        User user = userService.findById(1L);
+        System.out.println(user);
+    }
+
+    @Test
+    public void findAll() {
+        List<User> userList = userService.findAll();
+        System.out.println(userList);
+    }
+
+    @Test
+    public void deleteById() {
+        Long id = 1L;
+        userService.deleteById(id);
+        User user = userService.findById(id);
         System.out.println(user);
     }
 }
