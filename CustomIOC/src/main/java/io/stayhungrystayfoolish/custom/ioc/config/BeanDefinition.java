5861ab0545d81e89fd570dfdc0f95b7b34b9a4bc
StayHungryStayFoolish <bonismo@hotmail.com>
2019-08-22 10:58:12 +0800

    Add Custom IOC Framework

---
+++ CustomIOC/src/main/java/io/stayhungrystayfoolish/custom/ioc/config/BeanDefinition.java
@@ -0,0 +1,9 @@
+package io.stayhungrystayfoolish.custom.ioc.config;
+
+/**
+ * @Author: Created by bonismo@hotmail.com on 2019-08-22 10:57
+ * @Description:
+ * @Version: 1.0
+ */
+public class BeanDefinition {
+}
---
+++ CustomIOC/src/main/java/io/stayhungrystayfoolish/custom/ioc/factory/BeanFactory.java
@@ -0,0 +1,9 @@
+package io.stayhungrystayfoolish.custom.ioc.factory;
+
+/**
+ * @Author: Created by bonismo@hotmail.com on 2019-08-22 10:57
+ * @Description:
+ * @Version: 1.0
+ */
+public interface BeanFactory {
+}
