b080c9cbccbd6117170e99a12b13f1998dc03cf6
bonismo <bonismo@hotmail.com>
2019-10-17 15:59:33 +0800

    Custom MVC

---
+++ CustomMVC/src/main/java/io/stayhungrystayfoolish/custom/mvc/handler/HttpRequestHandler.java
@@ -0,0 +1,10 @@
+package io.stayhungrystayfoolish.custom.mvc.handler;
+
+/**
+ * @Author: bonismo@hotmail.com
+ * @Description:
+ * @Date: 2019-10-17 15:50
+ * @Version: V1.0
+ */
+public interface HttpRequestHandler {
+}
