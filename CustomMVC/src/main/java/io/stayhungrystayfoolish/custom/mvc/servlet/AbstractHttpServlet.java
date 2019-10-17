c343a1af90756321c760f9e9d2c131df27125f49
bonismo <bonismo@hotmail.com>
2019-10-18 02:47:58 +0800

    Custom MVC

--- CustomMVC/src/main/java/io/stayhungrystayfoolish/custom/mvc/servlet/AbstractHttpServlet.java
+++ CustomMVC/src/main/java/io/stayhungrystayfoolish/custom/mvc/servlet/AbstractHttpServlet.java
@@ -1,6 +1,10 @@
 package io.stayhungrystayfoolish.custom.mvc.servlet;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * @Author: bonismo@hotmail.com
@@ -8,5 +12,17 @@ import javax.servlet.http.HttpServlet;
  * @Date: 2019-10-18 01:37
  * @Version: V1.0
  */
-public class AbstractHttpServlet extends HttpServlet {
+public abstract class AbstractHttpServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doDispatch(req, resp);
+    }
+
+    public abstract void doDispatch(HttpServletRequest req, HttpServletResponse resp);
 }
---
+++ CustomMVC/src/main/java/io/stayhungrystayfoolish/custom/mvc/servlet/DispatcherServlet.java
@@ -0,0 +1,18 @@
+package io.stayhungrystayfoolish.custom.mvc.servlet;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @Author: bonismo@hotmail.com
+ * @Description:
+ * @Date: 2019-10-18 02:05
+ * @Version: V1.0
+ */
+public class DispatcherServlet extends AbstractHttpServlet {
+
+    @Override
+    public void doDispatch(HttpServletRequest req, HttpServletResponse resp) {
+
+    }
+}
