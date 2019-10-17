3afc914b7a2760b833057727fb3cb3abd1f23a14
bonismo <bonismo@hotmail.com>
2019-10-18 01:42:47 +0800

    Custom MVC

---
+++ CustomMVC/src/main/java/io/stayhungrystayfoolish/custom/mvc/servlet/AbstractHttpServlet.java
@@ -0,0 +1,12 @@
+package io.stayhungrystayfoolish.custom.mvc.servlet;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * @Author: bonismo@hotmail.com
+ * @Description:
+ * @Date: 2019-10-18 01:37
+ * @Version: V1.0
+ */
+public class AbstractHttpServlet extends HttpServlet {
+}
