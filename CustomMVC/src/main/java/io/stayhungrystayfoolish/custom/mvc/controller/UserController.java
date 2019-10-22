5d497319b2c8da4af47848989a2130e317b994ab
StayHungryStayFoolish <bonismo@hotmail.com>
2019-10-22 18:27:09 +0800

    Custom MVC

---
+++ CustomMVC/src/main/java/io/stayhungrystayfoolish/custom/mvc/controller/UserController.java
@@ -0,0 +1,32 @@
+package io.stayhungrystayfoolish.custom.mvc.controller;
+
+import io.stayhungrystayfoolish.custom.mvc.annotation.Controller;
+import io.stayhungrystayfoolish.custom.mvc.annotation.RequestMapping;
+import io.stayhungrystayfoolish.custom.mvc.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author: Created by bonismo@hotmail.com on 2019-10-22 17:42
+ * @Description:
+ * @Version: 1.0
+ */
+@Controller
+public class UserController {
+
+    @RequestMapping(value = "/query")
+    @ResponseBody
+    public Map<String, Object> query(Integer id, String username) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("id", id);
+        map.put("username", username);
+        return map;
+    }
+
+    @RequestMapping(value = "/delete")
+    @ResponseBody
+    public String delete(Integer id) {
+        return "OK";
+    }
+}
